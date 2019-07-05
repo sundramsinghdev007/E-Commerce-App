cc55f280934bc18ce2797d3d82ded79be0bfc95c
Annu1Singh <32551778+Annu1Singh@users.noreply.github.com>
2019-07-05 13:09:27 +0800

    Create AttributeEnumerable.java

---
+++ app/src/main/java/com/sundram/shardhafertilizer/bean/AttributeEnumerable.java
@@ -0,0 +1,10 @@
+package com.sundram.shardhafertilizer.bean;
+
+/**
+ * Created by Celery on 16/8/19.
+ *
+ */
+public interface AttributeEnumerable {
+    int getAttributeCountable();
+    Object getAttribute(int i);
+}
